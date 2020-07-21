11ae5b4f2b80baf8fc59bae45ad68a184bc54198
Michal Chromčák <michal.chromcak@heidelbergcement.com>
2020-07-21 16:57:28 +0200

    remove dead code

--- tests/unit/model_selection/test_prefect_executors.py
+++ tests/unit/model_selection/test_prefect_executors.py
@@ -10,73 +10,69 @@ from hcrystalball.model_selection import ModelSelectorResult
     indirect=["train_data", "grid_search"],
 )
 def test_prefect_executors(train_data, grid_search, parallel_columns):
-    try:
-        from prefect.engine.executors import DaskExecutor
-        from prefect.engine.executors import LocalDaskExecutor
-        from prefect.engine.executors import LocalExecutor
-        from dask.distributed import Client
-    except Exception:
-        print("`prefect` not installed, skipping the test...")
-        pass
-    else:
-        client = Client()
+    from prefect.engine.executors import DaskExecutor
+    from prefect.engine.executors import LocalDaskExecutor
+    from prefect.engine.executors import LocalExecutor
+    from dask.distributed import Client
 
-        executors = {
-            "dask_already_running": DaskExecutor(address=client.scheduler.address),
-            "local": LocalExecutor(),
-            "local_dask": LocalDaskExecutor(),
-            # this spins up LocalDaskExecutor, but just to check the interface
-            "dask_create_on_call": DaskExecutor(),
-        }
+    client = Client()
 
-        for executor_name, executor in executors.items():
-            flow, state = run_model_selection(
-                df=train_data,
-                grid_search=grid_search,
-                target_col_name="Quantity",
-                frequency="D",
-                partition_columns=["Product"],
-                parallel_over_columns=parallel_columns,
-                include_rules=None,
-                exclude_rules=None,
-                country_code_column="Holidays_code",
-                output_path="",
-                persist_cv_results=False,
-                persist_cv_data=False,
-                persist_model_reprs=False,
-                persist_best_model=False,
-                persist_partition=False,
-                persist_model_selector_results=False,
-                visualize_success=False,
-                executor=executor,
-            )
-            assert state.is_successful()
+    executors = {
+        "dask_already_running": DaskExecutor(address=client.scheduler.address),
+        "local": LocalExecutor(),
+        "local_dask": LocalDaskExecutor(),
+        # this spins up LocalDaskExecutor, but just to check the interface
+        "dask_create_on_call": DaskExecutor(),
+    }
 
-            results = select_model_general(
-                df=train_data,
-                grid_search=grid_search,
-                target_col_name="Quantity",
-                frequency="D",
-                partition_columns=["Product"],
-                parallel_over_columns=parallel_columns,
-                executor=executor,
-                include_rules=None,
-                exclude_rules=None,
-                country_code_column="Holidays_code",
-                output_path="",
-                persist_cv_results=False,
-                persist_cv_data=False,
-                persist_model_reprs=False,
-                persist_best_model=False,
-                persist_partition=False,
-                persist_model_selector_results=False,
-            )
+    for executor_name, executor in executors.items():
+        flow, state = run_model_selection(
+            df=train_data,
+            grid_search=grid_search,
+            target_col_name="Quantity",
+            frequency="D",
+            partition_columns=["Product"],
+            parallel_over_columns=parallel_columns,
+            include_rules=None,
+            exclude_rules=None,
+            country_code_column="Holidays_code",
+            output_path="",
+            persist_cv_results=False,
+            persist_cv_data=False,
+            persist_model_reprs=False,
+            persist_best_model=False,
+            persist_partition=False,
+            persist_model_selector_results=False,
+            visualize_success=False,
+            executor=executor,
+        )
+        assert state.is_successful()
 
-            assert len(results) == len(train_data[parallel_columns + ["Product"]].drop_duplicates())
-            assert isinstance(results[0], ModelSelectorResult)
+        results = select_model_general(
+            df=train_data,
+            grid_search=grid_search,
+            target_col_name="Quantity",
+            frequency="D",
+            partition_columns=["Product"],
+            parallel_over_columns=parallel_columns,
+            executor=executor,
+            include_rules=None,
+            exclude_rules=None,
+            country_code_column="Holidays_code",
+            output_path="",
+            persist_cv_results=False,
+            persist_cv_data=False,
+            persist_model_reprs=False,
+            persist_best_model=False,
+            persist_partition=False,
+            persist_model_selector_results=False,
+        )
 
-            if executor_name == "dask_already_running":
-                client.shutdown()
+        assert len(results) == len(train_data[parallel_columns + ["Product"]].drop_duplicates())
+        assert isinstance(results[0], ModelSelectorResult)
 
-        if client.status != "closed":
+        if executor_name == "dask_already_running":
             client.shutdown()
+
+    if client.status != "closed":
+        client.shutdown()
