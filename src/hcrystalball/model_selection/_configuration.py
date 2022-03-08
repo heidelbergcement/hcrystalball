7bb0ac8c7ac703f00c5c5ed127c8556b9748a79a
Michal Chromcak <michal.chromcak@real-digital.de>
2022-03-08 12:20:57 +0100

    lint

--- src/hcrystalball/model_selection/_configuration.py
+++ src/hcrystalball/model_selection/_configuration.py
@@ -135,9 +135,7 @@ def get_gridsearch(
         CV / Model selection configuration
     """
     exog_cols = exog_cols or []
-    country_code_columns = (
-        [country_code_column] if isinstance(country_code_column, str) else country_code_column
-    )
+    country_code_columns = [country_code_column] if isinstance(country_code_column, str) else country_code_column
     country_codes = [country_code] if isinstance(country_code, str) else country_code
 
     # ensures only exogenous columns and country code column will be passed to model if provided
@@ -181,9 +179,7 @@ def get_gridsearch(
     else:
         holiday = "passthrough"
 
-    estimator = Pipeline(
-        [("exog_passthrough", exog_passthrough), ("holiday", holiday), ("model", "passthrough")]
-    )
+    estimator = Pipeline([("exog_passthrough", exog_passthrough), ("holiday", holiday), ("model", "passthrough")])
 
     cv = FinerTimeSplit(n_splits=n_splits, horizon=horizon, between_split_lag=between_split_lag)
 
@@ -347,7 +343,7 @@ def get_gridsearch(
                 ],
                 "model__trend": ["add"],
                 "model__seasonal": [None, "add"],
-                "model__damped_trend": [True, False],                
+                "model__damped_trend": [True, False],
             }
         )
 
@@ -363,7 +359,7 @@ def get_gridsearch(
                 ],
                 "model__trend": ["add"],
                 "model__seasonal": ["mul"],
-                "model__damped_trend": [True, False],                
+                "model__damped_trend": [True, False],
             }
         )
 
@@ -379,7 +375,7 @@ def get_gridsearch(
                 ],
                 "model__trend": [None],
                 "model__seasonal": [None, "add", "mul"],
-                "model__damped_trend": [False],               
+                "model__damped_trend": [False],
             }
         )
 
